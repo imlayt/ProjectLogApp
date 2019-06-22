2078027915f29f0256172ac10f190e330a9db5a8
Tom Imlay <imlayt@gmail.com>
2019-06-22 11:36:00 -0500

    Roughing out the framework. Start adding in the GUI.
    6/19/2019 tom
    added ability to browse for a file and enter a tablename in case the
    defaults don't work.
    
    added more error checking for the file and table names
    
    added setmessage function - writes to the _MESSAGEAREA_ text field at
    the bottom of the window.
    
    Added most of the fields.
    Added record selector listbox. 2019-06-22

--- ProjectLogApp.py
+++ ProjectLogApp.py
@@ -16,11 +16,13 @@ from sqlite3 import Error
 
 # thelogfilehome = 'C:\Users\imlay\Downloads\ProjectLog.db'
 
-thelogfile = 'c:/Users/imlay/Downloads/ProjectLog2.db'  # Default log file
-thelogtable = 'LogEntries2'  # Default tablename
+thelogfile = 'c:/Users/imlay/Downloads/ProjectLog.db'  # Default log file
+thelogtable = 'LogEntries'  # Default tablename
 lightblue = '#b9def4'  # color used by PySimpleGUI
 mediumblue = '#d2d2df'  # color used by PySimpleGUI
 mediumblue2 = '#534aea'  # color used by PySimpleGUI
+
+recordlist = []  # list of records for the selector listbox
 
 class ProjectLog:
     def __init__(self, logfile, table='LogEntries'):
@@ -30,11 +32,12 @@ class ProjectLog:
         logtype = ''  # Issue, risk, action item, decision, etc.
         title = ''  # short name for reporting
         shortdescription = ''  # short description for reporting
-        longdescription = ''  # Detailed description
+        longdescription = ''  # optional detailed description
         probability = 0.0  # likely hood of a risk
         impact = 0.0  # impact to the project of the risk or issue
         complexity = 0.0  # complexity multiplier for Risks and issues
-        exposure = 0.0  # calculated expodure of the project based on probability, impact, and complexity
+        criticality = 0.0  # criticality multiplier for Risks and Issues
+        exposure = 0.0  # calculated exposure of the project based on probability, impact, complexity and criticality
         owner = ''  # Person accountable for mitigating the risk, remediating the issue, etc.
         startdate = ''  # Planned date for starting the mitigation/remediation
         duedate = ''  # Target finish date for mitigation/remediation
@@ -112,22 +115,22 @@ class ProjectLog:
             sg.Popup('Creating table FAILED(', self.table, ')', keep_on_top=True)
             return False
 
-    def addlogentry(self):  # returns True if the addition was successful
+    def addlogentry(self, valuelist):  # returns the ID value if the addition was successful else returns None
         pass
 
     def readlogentry(self):  # returns a log record
         pass
 
-    def updatelogentry(self):  # returns True if the update was successful
+    def updatelogentry(self, logidvalue, valuelist):  # returns True if the update was successful
         pass
 
-    def deletelogentry(self):  # returns True if the record was deleted
+    def deletelogentry(self, logidvalue):  # returns True if the record was deleted
         pass
 
-    def findlogentry(self):  # returns a log record
+    def findlogentry(self, field, searchvalue):  # returns a log record
         pass
 
-    def reportlogentries(self):  # returns a list of log records
+    def reportlogentries(self, searchquery):  # returns a list of log records
         pass
 
 
@@ -136,12 +139,21 @@ def setmessage(window, message):
     window.Refresh()
 
 
+def fillrecordselector(window, fieldkey, valuelist):
+    pass
+
+
 maincolumn1 = [[sg.Text('Title', size=(15, 1), justification='right'), sg.Multiline(size=(40, 3), key='_TITLE_')],
                [sg.Text('Short Desc.', size=(15, 1), justification='right'),
                 sg.Multiline(size=(40, 3), key='_SHORTDESC_')],
-               [sg.Text('Long Desc.', size=(15, 1), justification='right'),
+               [sg.Text('Long Desc.(opt)', size=(15, 1), justification='right'),
                 sg.Multiline(size=(40, 3), key='_LONGDESC_')],
-               [sg.Text('Notes', size=(15, 1), justification='right'), sg.Multiline(size=(40, 3), key='_NOTES_')]]
+               [sg.Text('Project.', size=(15, 1), justification='right'),
+                sg.InputText(size=(40, 1), key='_PROJECT_')],
+               [sg.Text('Workstream.', size=(15, 1), justification='right'),
+                sg.InputText(size=(40, 1), key='_WORKSTREAM_')],
+               [sg.Text('Notes', size=(15, 1), justification='right'),
+                sg.Multiline(size=(40, 4), key='_NOTES_')]]
 
 maincolumn2 = [[sg.Text('Log Item Type', size=(15, 1), justification='right'),
                 sg.InputText(size=(20, 1), key='_LOGITEMTYPE_')],
@@ -150,22 +162,27 @@ maincolumn2 = [[sg.Text('Log Item Type', size=(15, 1), justification='right'),
                [sg.Text('Severity', size=(15, 1), justification='right'), sg.InputText(size=(20, 1), key='_SEVERITY_')],
                [sg.Text('Complexity', size=(15, 1), justification='right'),
                 sg.InputText(size=(20, 1), key='_COMPLEXITY_')],
-               [sg.Text('Exposure', size=(15, 1), justification='right'), sg.InputText(size=(20, 1), key='_EXPOSURE_')],
-               [sg.Text('Log Item Type', size=(15, 1), justification='right'),
-                sg.InputText(size=(20, 1), key='_LOGITEMTYPE_')],
+               [sg.Text('Exposure', size=(15, 1), justification='right'), sg.InputText(size=(20, 1), key='_EXPOSURE_')]
                ]
 
 maincolumn3 = [[sg.Text('Owner', justification='right', size=(15, 1)), sg.InputText(size=(20, 1), key='_OWNER_')],
-               [sg.Text('Owner', justification='right', size=(15, 1)), sg.InputText(size=(20, 1), key='_OWNER_')],
-               [sg.Text('Start', justification='right', size=(15, 1)), sg.InputText(size=(20, 1), key='_START_')],
-               [sg.Text('Due Date', justification='right', size=(15, 1)), sg.InputText(size=(20, 1), key='_DUEDATE_')],
-
+               [sg.Text('tbd', justification='right', size=(15, 1)), sg.InputText(size=(20, 1), key='_TBD_')],
+               [sg.Text('Start', justification='right', size=(15, 1)), sg.InputText(size=(20, 1), key='_START_'),
+                sg.CalendarButton('Cal', target='_START_')],
+               [sg.Text('Due Date', justification='right', size=(15, 1)), sg.InputText(size=(20, 1), key='_DUEDATE_'),
+                sg.CalendarButton('Cal', target='_DUEDATE_')],
                ]
+
+maincolumn4 = [[sg.Column(maincolumn2, background_color=mediumblue),
+                sg.Column(maincolumn3, background_color=mediumblue)],
+               [sg.Text('Record Selector', justification='center', size=(60, 1))],
+               [sg.Listbox(values=recordlist, size=(87, 8), key='_RECORDSELECTOR_')]
+               ]
+
 fileinfo = thelogfile + '  |  ' + thelogtable
 # Define the mainscreen layout using the above layouts
-mainscreenlayout = [[sg.Column(maincolumn1, background_color=mediumblue),
-                     sg.Column(maincolumn2, background_color=mediumblue),
-                     sg.Column(maincolumn3, background_color=mediumblue)],
+mainscreenlayout = [[sg.Column(maincolumn1, background_color=lightblue),
+                     sg.Column(maincolumn4, background_color=mediumblue2)],
                     [sg.Text('Message Area', size=(131, 1), key='_MESSAGEAREA_')],
                     [sg.Button('Add New', key='_ADDNEW_'),
                      sg.Button('Save Changes', key='_SAVECHANGES_'),
@@ -187,55 +204,74 @@ def main():
     # instantiate a ProjectLog
     mylog = ProjectLog(thelogfile, thelogtable)
 
+    if mylog.verifylogfile() is None:
+        sg.Popup('ERROR: Could not open the logfile')
+        logfile = sg.PopupGetFile('Enter a logfile')
+        print('logfile => ', logfile)
+
+        if logfile is None or len(logfile)==0:
+            print('logfile not found')
+            # break
+        else:
+            mylog.logfile = logfile
+            print('mylog.table => ', mylog.table)
+            if not mylog.verifylogfile():
+                sg.Popup('2nd file verification failed - EXITING Program')
+                # break
+            else:
+                # sg.Popup('Opened the logfile')
+                fileinfo = mylog.logfile + '  |  ' + mylog.table
+                window.FindElement('_FILEINFO_').Update(fileinfo)
+                setmessage(window, fileinfo)
+                window.Refresh()
+    # else:
+    # sg.Popup('Opened the logfile')
+
+    if not mylog.verifytable():
+        logtable = sg.PopupGetText('Enter a tablename or Cancel to exit the program.')
+        print('logtable => ', logtable)
+
+        if logtable is None or len(logtable)==0:
+            print('logtable not found')
+            # break
+        else:
+            mylog.table = logtable
+            print('mylog.table => ', mylog.table)
+            if not mylog.verifytable():
+                sg.Popup('2nd table verification failed - EXITING Program')
+                # break
+            else:
+                # sg.Popup('connected to the table')
+                fileinfo = mylog.logfile + '  |  ' + mylog.table
+                window.FindElement('_FILEINFO_').Update(fileinfo)
+                setmessage(window, fileinfo)
+                window.Refresh()
+    # else:
+    # sg.Popup('connected to the table')
+
+    if fillrecordselector(window, 'fieldkey', 'valuelist'):
+        sg.Popup('fillrecordselector SUCCEDED')
+        # pop the list and load the first record into the input boxes
+    else:
+        sg.Popup('fillrecordselector FAILED')
+        # sys.exit(1)
+
     while True:  # Event Loop
         event, values = window.Read()
         if event is None or event=="Exit":
             sys.exit(1)
 
-        if mylog.verifylogfile() is None:
-            sg.Popup('ERROR: Could not open the logfile')
-            logfile = sg.PopupGetFile('Enter a logfile')
-            print('logfile => ', logfile)
+        if event=='_ADDNEW_':
+            sg.Popup('Add New Button')
 
-            if logfile is None or len(logfile)==0:
-                print('logfile not found')
-                break
-            else:
-                mylog.logfile = logfile
-                print('mylog.table => ', mylog.table)
-                if not mylog.verifylogfile():
-                    sg.Popup('2nd file verification failed - EXITING Program')
-                    break
-                else:
-                    sg.Popup('Opened the logfile')
-                    fileinfo = mylog.logfile + '  |  ' + mylog.table
-                    window.FindElement('_FILEINFO_').Update(fileinfo)
-                    setmessage(window, fileinfo)
-                    window.Refresh()
-        else:
-            sg.Popup('Opened the logfile')
+        if event=='_SAVECHANGES_':
+            sg.Popup('Save Changes Button')
 
-        if not mylog.verifytable():
-            logtable = sg.PopupGetText('Enter a tablename or Cancel to exit the program.')
-            print('logtable => ', logtable)
+        if event=='_CANCEL_':
+            sg.Popup('Cancel Button')
 
-            if logtable is None or len(logtable)==0:
-                print('logtable not found')
-                break
-            else:
-                mylog.table = logtable
-                print('mylog.table => ', mylog.table)
-                if not mylog.verifytable():
-                    sg.Popup('2nd table verification failed - EXITING Program')
-                    break
-                else:
-                    sg.Popup('connected to the table')
-                    fileinfo = mylog.logfile + '  |  ' + mylog.table
-                    window.FindElement('_FILEINFO_').Update(fileinfo)
-                    setmessage(window, fileinfo)
-                    window.Refresh()
-        else:
-            sg.Popup('connected to the table')
+        if event=='_FILEINFO_':
+            sg.Popup('File Info Button')
 
 
 if __name__=="__main__":
